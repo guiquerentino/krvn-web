0ec207e38f7a4880fb3584e54d67956a59d63ac6
guiquerentino <squerentino@gmail.com>
2025-09-24 15:28:53 -0300

    feat: header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { AboutUs } from './pages/about-us/about-us';
+import { ContactUs } from './pages/contact-us/contact-us';
+import { Products } from './pages/products/products';
+import { Careers } from './pages/careers/careers';
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'quem-somos',
+        component: AboutUs
+    },
+    {
+        path: 'contato',
+        component: ContactUs
+    },
+    {
+        path: 'produtos',
+        component: Products
+    },
+    {
+        path: 'vagas',
+        component: Careers
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
