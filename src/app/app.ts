196a5e061d1c82ae83a4388ac77a7d2c44a7cb3e
guiquerentino <squerentino@gmail.com>
2025-09-24 15:39:17 -0300

    ajustando components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Footer } from './components/footer/footer';
+import { Footer } from './components/footer/footer.component';
 import { Header } from './components/header/header';
 
 @Component({
