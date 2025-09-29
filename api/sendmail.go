a299bce109e0777c073a98ece97de8f13598d7e5
guiquerentino <squerentino@gmail.com>
2025-09-29 14:29:51 -0300

    fix: options

--- api/sendmail.go
+++ api/sendmail.go
@@ -15,6 +15,11 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
 	if r.Method != http.MethodPost {
 		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
 		return
